014e53c200b14f8e891cf90f9fea2dad495885c6
root <root@Honda.motoracing.org>
2016-07-01 09:59:56 -0500

    added some more code
    fixed to escape gt and lt
    
    finally got it working correctly all together

--- public/client.js
+++ public/client.js
@@ -1,10 +1,27 @@
 $(function(){
 
-  $.get('/api', searchFunction);
-
-  funtion searchFunction(search) {
-    var quote = search;
-    var finalQuote = "<tr><td>" + quote.title + ": " + quote.quote + "</tr></td>";
-    $('table').append(finalQuote);
-  }
-}); 
+  $("#button").click( function(){
+    var input = $("#myInput").val();
+    $.get('/api?search=' + input, function(search) {
+      var quote = search.title + ": " + search.quote;
+      var quoted = quote.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+      var finalQuote = "<h2 id=\"tablerow\" class=\"row\">" + quoted + "</h2>";
+      if (search === ""){
+        var badInput = "What the hell are you doing?";
+        var badQuote = "<h2 id=\"tablerow\" class=\"row\">" + badInput + "</h2>";
+        $('.row').remove();
+        setTimeout(function() {
+          $("#search").append(badQuote);}, 250);
+      } else {
+        $('h2').remove();
+        $('#search').append(finalQuote);
+      }
+    });
+  });
+  $('#myInput').on("keydown", function search(e){
+    if (e.keyCode == 13){
+      e.preventDefault();
+      $('#button').click();
+    }
+  });
+});
